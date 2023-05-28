e25dc804c79167702994846e1152163f48c92012
22P31A0427 <119720707+22P31A0427@users.noreply.github.com>
2023-05-28 18:57:01 +0530

    #include<stdio.h> int main() {     int a,b,c,i,n;     scanf("%d",&n);     a=0;     b=1;     printf("%d ",a);     printf("%d ",b);     for(i=0;i<n-2;i++)     {      c=a+b;      a=b;      b=c;
         printf("%d ",c);
        }
    }

---
+++ Play_with_Fibonacci_sequence.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,i,n;
+    scanf("%d",&n);
+    a=0;
+    b=1;
+    printf("%d ",a);
+    printf("%d ",b);
+    for(i=0;i<n-2;i++)
+    {
+     c=a+b;
+     a=b;
+     b=c;
+     
+     
+     printf("%d ",c);
+    }
+}
